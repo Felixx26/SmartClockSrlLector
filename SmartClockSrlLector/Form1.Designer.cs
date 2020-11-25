2af23660ad10b21756fb8af5a2e344cc201d01ef
Felixx26 <felixxrafaell@gmail.com>
2020-11-25 09:58:46 -0400

    Agregar archivos de proyecto.

---
+++ SmartClockSrlLector/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace SmartClockSrlLector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cmbUsersDevelop = new System.Windows.Forms.ComboBox();
+            this.panelFinger = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelInformation = new System.Windows.Forms.Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.datehour = new System.Windows.Forms.Timer(this.components);
+            this.timerProfile = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panelFinger.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panelInformation.SuspendLayout();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Controls.Add(this.lblHour);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 441);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblDate.Location = new System.Drawing.Point(38, 233);
+            this.lblDate.MaximumSize = new System.Drawing.Size(400, 35);
+            this.lblDate.MinimumSize = new System.Drawing.Size(400, 35);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(400, 35);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Saturday, May 23 2019";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("Verdana", 92F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHour.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblHour.Location = new System.Drawing.Point(26, 84);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(431, 149);
+            this.lblHour.TabIndex = 0;
+            this.lblHour.Text = "16:06";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel2.Controls.Add(this.cmbUsersDevelop);
+            this.panel2.Controls.Add(this.panelFinger);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(488, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 441);
+            this.panel2.TabIndex = 1;
+            // 
+            // cmbUsersDevelop
+            // 
+            this.cmbUsersDevelop.FormattingEnabled = true;
+            this.cmbUsersDevelop.Location = new System.Drawing.Point(290, 417);
+            this.cmbUsersDevelop.Name = "cmbUsersDevelop";
+            this.cmbUsersDevelop.Size = new System.Drawing.Size(48, 21);
+            this.cmbUsersDevelop.TabIndex = 4;
+            // 
+            // panelFinger
+            // 
+            this.panelFinger.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panelFinger.Controls.Add(this.pictureBox2);
+            this.panelFinger.Location = new System.Drawing.Point(119, 255);
+            this.panelFinger.Name = "panelFinger";
+            this.panelFinger.Size = new System.Drawing.Size(119, 151);
+            this.panelFinger.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SmartClockSrlLector.Properties.Resources.fingerprint;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(119, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.panel3.Controls.Add(this.panelInformation);
+            this.panel3.Controls.Add(this.lblError);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(350, 220);
+            this.panel3.TabIndex = 2;
+            // 
+            // panelInformation
+            // 
+            this.panelInformation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.panelInformation.Controls.Add(this.lblStatus);
+            this.panelInformation.Controls.Add(this.lblName);
+            this.panelInformation.Controls.Add(this.label7);
+            this.panelInformation.Controls.Add(this.lblPosition);
+            this.panelInformation.Controls.Add(this.label11);
+            this.panelInformation.Controls.Add(this.label12);
+            this.panelInformation.Controls.Add(this.panel6);
+            this.panelInformation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelInformation.Location = new System.Drawing.Point(0, 0);
+            this.panelInformation.Name = "panelInformation";
+            this.panelInformation.Size = new System.Drawing.Size(350, 220);
+            this.panelInformation.TabIndex = 4;
+            this.panelInformation.Visible = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(196, 64);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(93, 16);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Felix Garcia";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(140, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Name:";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPosition.Location = new System.Drawing.Point(227, 96);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(106, 16);
+            this.lblPosition.TabIndex = 3;
+            this.lblPosition.Text = "Desarrollador";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(140, 96);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 16);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Ocupation:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(7, 13);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(94, 18);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "My profile";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.picImage);
+            this.panel6.Location = new System.Drawing.Point(6, 39);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(130, 130);
+            this.panel6.TabIndex = 0;
+            // 
+            // picImage
+            // 
+            this.picImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picImage.Image = global::SmartClockSrlLector.Properties.Resources.usuario;
+            this.picImage.Location = new System.Drawing.Point(0, 0);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(130, 130);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImage.TabIndex = 0;
+            this.picImage.TabStop = false;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblError.Location = new System.Drawing.Point(32, 96);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(286, 18);
+            this.lblError.TabIndex = 8;
+            this.lblError.Text = "A error has ocurred. Try againg";
+            this.lblError.Visible = false;
+            // 
+            // datehour
+            // 
+            this.datehour.Enabled = true;
+            this.datehour.Tick += new System.EventHandler(this.datehour_Tick);
+            // 
+            // timerProfile
+            // 
+            this.timerProfile.Interval = 3000;
+            this.timerProfile.Tick += new System.EventHandler(this.timerProfile_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(140, 153);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(66, 16);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Check In";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 441);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panelFinger.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panelInformation.ResumeLayout(false);
+            this.panelInformation.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelFinger;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Panel panelInformation;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Timer datehour;
+        private System.Windows.Forms.ComboBox cmbUsersDevelop;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Timer timerProfile;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ SmartClockSrlLector/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartClockSrlLector.Model;
+
+namespace SmartClockSrlLector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<int> Cache = new List<int>() {-1};
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            using (smartclocksrldbEntities db = new smartclocksrldbEntities())
+            {
+                employees oEmployees = db.employees.Find((int)cmbUsersDevelop.SelectedValue);
+                if (oEmployees != null)
+                {
+                    lblName.Text = oEmployees.name;
+                    lblPosition.Text = oEmployees.position.name;
+                    if (oEmployees.images != null)
+                    {
+                        MemoryStream ms = new MemoryStream(oEmployees.images);
+                        Image returnImage = Image.FromStream(ms);
+                        picImage.Image = returnImage;
+                    }
+                    if (registerEmployee(oEmployees))
+                    {
+                        panelFinger.BackColor = Color.ForestGreen;
+                        panelInformation.Visible = true;
+                        lblError.Visible = false;
+                        timerProfile.Start();
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                    }
+                }
+                else
+                {
+                    lblError.Visible = true;
+                }
+            }
+                
+        }
+
+        private void datehour_Tick(object sender, EventArgs e)
+        {
+            lblHour.Text = DateTime.Now.ToString("HH:mm");
+            lblDate.Text = DateTime.Now.ToString("dddd, MMM dd, yyyy");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (smartclocksrldbEntities db = new smartclocksrldbEntities())
+            {
+                var lst = from d in db.employees
+                          select d;
+                cmbUsersDevelop.DataSource = lst.ToList();
+                cmbUsersDevelop.DisplayMember = "id";
+                cmbUsersDevelop.ValueMember = "id";
+            }
+        }
+
+        private void timerProfile_Tick(object sender, EventArgs e)
+        {
+            panelFinger.BackColor = Color.FromArgb(40,40,40);
+            panelInformation.Visible = false;
+            lblError.Visible = false;
+            timerProfile.Stop();
+        }
+
+        private bool registerEmployee(employees e)
+        {
+            try
+            {
+                using (smartclocksrldbEntities db = new smartclocksrldbEntities())
+                {
+                    if (!this.Cache.Contains(e.id))
+                    {
+                        checkin oCheckin = new checkin
+                        {
+                            employeeID = e.id,
+                            checkDate = DateTime.Now.Date,
+                            checkHour = DateTime.Now.ToString("HH:mm:ss"),
+                            observations = null
+                        };
+                        db.checkin.Add(oCheckin);
+                        db.SaveChanges();
+                        lblStatus.Text = "Check In";
+                        lblStatus.ForeColor = Color.LightGreen;
+                        this.Cache.Add(e.id);
+                        return true;
+                    }
+                    else
+                    {
+                        checkout oCheckout = new checkout
+                        {
+                            employeeID = e.id,
+                            checkDate = DateTime.Now.Date,
+                            checkHour = DateTime.Now.ToString("HH:mm:ss"),
+                            observations = null
+                        };
+                        db.checkout.Add(oCheckout);
+                        db.SaveChanges();
+                        lblStatus.Text = "Check Out";
+                        lblStatus.ForeColor = Color.LightGreen;
+                        this.Cache.Remove(e.id);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
